91adccadfd555184aa375b2c4748516231973976
Vasu Goel <vasu18322@iiitd.ac.in>
2024-04-06 05:38:01 +0530

    Added initial set of controls for testing code

--- client/src/GameComponentScreen.tsx
+++ client/src/GameComponentScreen.tsx
@@ -1,48 +1,104 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { useDojo } from "./dojo/useDojo";
-import image1 from "../src/assets/1.png";
+import {Game2Message} from "./Constants"
+
 type GameComponentScreenProps = {
-  gameState: number;
-};
+    gameState: number,
+    startGame: () => void,
+    createGame: () => void,
+    joinGame: () => void
+}
+
+function GameState1Components(props: GameComponentScreenProps) {
+    return (
+        <>
+            <div className="controller-container">
+                <button onclick={() => props.startGame()}>Start Game</button>
+            </div>
+        </>
+    )
+}
+
+function GameState2Components(props: GameComponentScreenProps) {
+    return (
+        <>
+            <div className="controller-column">
+                <div className="form-field">
+                    <label className="label" for="name">Enter your name: </label>
+                    <input placeholder="Please enter your name" id="name" className="input" />
+                </div>
+                <div className="controller-container">
+                    <button onclick={() => props.createGame()}>Create Game</button>
+                    <button onclick={() => props.joinGame()}>Join Game</button>
+                </div>
+            </div>
+        </>
+    )
+}
 
 function GameComponentScreen(props: GameComponentScreenProps) {
-  const [backgroundImage, setBackgroundImage] = useState("./assets/1.png");
-  const [gameText, setGameText] = useState("Welcome to Crypto Condundrum");
-  const [gameState, setGameState] = useState(0);
+    
+    const [backgroundImage, setBackgroundImage] = useState("./assets/1.png")
+    const [gameText, setGameText] = useState("Welcome to Crypto Condundrum")
+    const [gameControls, setGameControls] = useState(<></>)
+    const [gameState, setGameState] = useState(0);
 
-  function getGameTextBasedOnGameState(gameState: number) {
-    switch (gameState) {
-      case 1:
-        return "Welcome to Crypto Conundrum";
-        break;
-      default:
-        return "Welcome to Crypto Conundrum";
-        break;
+    function getGameTextBasedOnGameState(gameState: number) {
+        switch (gameState) {
+            case 1:
+                return "Welcome to Crypto Conundrum";
+                break;
+            case 2: 
+                return Game2Message;
+            default:
+                return "Welcome to Crypto Conundrum";
+                break;
+        }
+        return "Welcome to Crypto Conundrum"
     }
-    return "Welcome to Crypto Conundrum";
-  }
 
-  useEffect(() => {
-    setBackgroundImage(`../src/assets/${props.gameState}.png`);
-    // setBackgroundImage("../../");
-    setGameText(getGameTextBasedOnGameState(props.gameState));
-  }, [props.gameState]);
+    function getGameControls(gameState: number) {
+        switch (gameState) {
+            case 1: 
+                return GameState1Components()
+            case 2:
+                return GameState2Components()
+            default:
+                return GameState1Components()
+                break;
+        }
+    }
 
-  return (
-    <>
-      <div className="gameComponentScreenClass">
-        <div className="imageHolder" style={{ height: "100%", width: "100%" }}>
-          <img
-            style={{ height: "100%", width: "100%" }}
-            src={backgroundImage}
-          />
-        </div>
-        <div className="gameTextPrompt">{gameText}</div>
-        <div className="gameControls">No actions to perform right now</div>
-      </div>
-    </>
-  );
+    useEffect(() => {
+        setBackgroundImage(`../src/assets/${props.gameState}.png`)
+        setGameText(getGameTextBasedOnGameState(props.gameState))
+        setGameControls(getGameControls(props.gameState))
+    }, [props.gameState])
+
+    return (
+        <>
+            <div className="gameComponentScreenClass">
+                <div className="imageHolder" style={{"height": "100%", "width": "100%"}}>
+                    <img style={{"height": "100%", "width": "100%"}} src={backgroundImage} />
+                </div>
+                <div className="gameTextPrompt">
+                    {gameText}
+                </div>
+                <div className="gameControls">
+                    {gameControls}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default GameComponentScreen;
+
+
+// Game flow plan
+/*
+1. Welcome screen
+2. Inputs: Name | Buttons: Create game and Join Game
+
+*/
